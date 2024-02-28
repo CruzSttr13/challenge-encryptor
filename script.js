0b4b39a97e3fe9dc89ec7bccf688fc249a7f5097
cruzsttr13 <dev.estrella.jc@gmail.com>
2024-02-28 13:41:26 -0500

    best practices and no mayus

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Function to process text based on user choice
 function processText() {
   var text = document.getElementById("text").value.toLowerCase();
   var option = document.querySelector('input[name="option"]:checked');
@@ -8,7 +9,7 @@ function processText() {
       title: 'Oops...',
       text: 'Please select an option (Encrypt or Decrypt) before processing.',
     });
-    return; // Detener la ejecución si no se ha seleccionado ninguna opción
+    return;
   }
 
   var result = "";
@@ -22,7 +23,13 @@ function processText() {
   document.getElementById("result").value = result;
 }
 
+//fUNCTION TO CONVERT TEXT TO LOWERCASE
+document.getElementById("text").addEventListener("input", function() {
+  this.value = this.value.toLowerCase();
+});
 
+
+// Function to encrypt text
 function encrypt(text) {
   text = text.replaceAll("e", "enter");
   text = text.replaceAll("i", "imes");
@@ -32,6 +39,7 @@ function encrypt(text) {
   return text;
 }
 
+// Function to decrypt text
 function decrypt(text) {
   text = text.replaceAll("imes", "i");
   text = text.replaceAll("ober", "o");
@@ -41,6 +49,7 @@ function decrypt(text) {
   return text;
 }
 
+// Function to copy result to clipboard
 function copyToClipboard() {
   var copyText = document.getElementById("result");
 
@@ -52,7 +61,7 @@ function copyToClipboard() {
     });
   } else {
     copyText.select();
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+    copyText.setSelectionRange(0, 99999);
     document.execCommand("copy");
 
     Swal.fire({
@@ -62,67 +71,40 @@ function copyToClipboard() {
       allowOutsideClick: false,
       showCloseButton: true,
     }).then((result) => {
-      // Verificar si el usuario cerró la alerta manualmente
       if (result.isConfirmed || result.isDismissed) {
-        // Resetear la textarea
         copyText.value = "";
       }
     });
   }
 }
 
-
-
-
-//background.js
-
-// geting canvas by Boujjou Achraf
+// Background animation
 var c = document.getElementById("c");
 var ctx = c.getContext("2d");
-
-//making the canvas full screen
 c.height = window.innerHeight;
 c.width = window.innerWidth;
-
-//chinese characters - taken from the unicode charset
-var matrix =
-  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%+-/~{[|`]}";
-//converting the string into an array of single characters
+var matrix = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%+-/~{[|`]}";
 matrix = matrix.split("");
-
 var font_size = 10;
-var columns = c.width / font_size; //number of columns for the rain
-//an array of drops - one per column
+var columns = c.width / font_size;
 var drops = [];
-//x below is the x coordinate
-//1 = y co-ordinate of the drop(same for every drop initially)
 for (var x = 0; x < columns; x++) drops[x] = 1;
 
-//drawing the characters
 function draw() {
-  //Black BG for the canvas
-  //translucent BG to show trail
   ctx.fillStyle = "rgba(0, 0, 0, 0.04)";
   ctx.fillRect(0, 0, c.width, c.height);
 
-  ctx.fillStyle = "#034a10"; //green text
+  ctx.fillStyle = "#034a10";
   ctx.font = font_size + "px arial";
-  //looping over drops
+
   for (var i = 0; i < drops.length; i++) {
-    //a random chinese character to print
     var text = matrix[Math.floor(Math.random() * matrix.length)];
-    //x = i*font_size, y = value of drops[i]*font_size
     ctx.fillText(text, i * font_size, drops[i] * font_size);
 
-    //sending the drop back to the top randomly after it has crossed the screen
-    //adding a randomness to the reset to make the drops scattered on the Y axis
     if (drops[i] * font_size > c.height && Math.random() > 0.975) drops[i] = 0;
 
-    //incrementing Y coordinate
     drops[i]++;
   }
 }
 
 setInterval(draw, 35);
-
-
